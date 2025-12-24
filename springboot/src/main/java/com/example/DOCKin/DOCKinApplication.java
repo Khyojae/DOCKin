b6ad3ab953d5b5ef0f2237dc7cce45332ae0e406
Khyojae <khjae201@gmail.com>
2025-12-24 14:35:43 +0900

    db일단끔

--- springboot/src/main/java/com/example/DOCKin/DOCKinApplication.java
+++ springboot/src/main/java/com/example/DOCKin/DOCKinApplication.java
@@ -2,12 +2,13 @@ package com.example.DOCKin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class DOCKinApplication {
 
 	public static void main(String[] args) {
